58572eb6172c17a93235827f68adc5df95fa752e
Laphatize <laphatize.me@gmail.com>
2017-11-14 19:50:35 -0500

    Update validate.js

--- javascript/validate.js
+++ javascript/validate.js
@@ -1,5 +1,6 @@
 function check() {
 
-var possibleWebsites = [www.wikipedia.org,  www.bio.com, www.cnn.com , pie.com, laphatize.me/newsletChecky]
+var possibleWebsites = [www.wikipedia.org,  www.bio.com, www.cnn.com , pie.com, laphatize.me/newsletChecky];
+ 
 
 }
