8e6adbb09e753ebbc5ddc24d5ac8098ec37faf35
Laphatize <pran.pub@gmail.com>
2017-11-18 12:27:57 -0500

    Added Code

--- javascript/getWebsite.js
+++ javascript/getWebsite.js
@@ -2,3 +2,23 @@ function requestSiteName() {
 var websiteUrl = window.prompt("Please enter the website URL.")
 console.log(websiteUrl);
 }
+
+
+function tryURL() {
+
+var possibleWebsites = [
+
+"www.wikipedia.com",
+"https://bbc.com",
+"http://laphatize.me/testing",
+"www.cnn.com"
+
+]
+
+if (possibleWebsites == websiteUrl)
+	window.alert("This website has valid content")
+} else {
+
+window.alert("The website you entered has invalid content.")
+
+}
